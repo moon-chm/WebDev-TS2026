d42f0664e4e3e970d7c1630966172495fe6a37eb
moon-chm <your-github-email@example.com>
2026-01-15 20:32:11 +0530

    bubble effects

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,45 @@ import { AppleLiquidGlassNav } from './components/AppleNav'
 import { BlurFade } from "./components/ui/blur-fade"
 import { ProgressiveBlur } from "./components/progressiveblur"
 import { Skiper17 } from "./components/cardscroll"
+import SplineScene from "./components/SplineScene"
 
 function App() {
   return (
     <div className="min-h-screen bg-black relative">
-        <AppleLiquidGlassNav />
-      <ProgressiveBlur position="top" backgroundColor="rgba(0,0,0,0.3)" height="100px" blurAmount="8px" className="z-10" />
-      <BlurFade>    
-        
-        <SyntheticHero 
+      <AppleLiquidGlassNav />
+
+      <ProgressiveBlur
+        position="top"
+        backgroundColor="rgba(0,0,0,0.3)"
+        height="100px"
+        blurAmount="8px"
+        className="z-10"
+      />
+
+      <BlurFade>
+        <SyntheticHero
           title="WHERE TOP ENGINERS RISE"
           description="A national-level technical event by DKTE."
         />
       </BlurFade>
-      
+{/* Spline Section (AFTER Sticky Cards) */}
+      <div className="h-screen relative">
+        <SplineScene />
+      </div>
       {/* Sticky Card Scroll Section */}
       <div className="h-screen relative bg-black">
         <Skiper17 />
       </div>
+
       
-      <ProgressiveBlur position="bottom" backgroundColor="rgba(0,0,0,0.3)" height="100px" blurAmount="8px" className="z-10" />
+
+      <ProgressiveBlur
+        position="bottom"
+        backgroundColor="rgba(0,0,0,0.3)"
+        height="100px"
+        blurAmount="8px"
+        className="z-10"
+      />
     </div>
   )
 }
---
+++ src/components/SplineScene.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline'
+
+export default function SplineScene() {
+  return (
+    <div className="w-full h-full">
+      <Spline scene="https://prod.spline.design/cZ2GGUq2CYKw0X8m/scene.splinecode" />
+    </div>
+  )
+}
