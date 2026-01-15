c1cb2a2b0337e2d1e6498e28eb85c083496d51f3
moon-chm <your-github-email@example.com>
2026-01-15 20:54:47 +0530

    speed insight added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { AppleLiquidGlassNav } from './components/AppleNav'
 import { BlurFade } from "./components/ui/blur-fade"
 import { Skiper17 } from "./components/cardscroll"
 import SplineScene from "./components/SplineScene"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 function App() {
   return (
     <div className="min-h-screen bg-black relative overflow-hidden">
@@ -27,6 +29,9 @@ function App() {
       <div className="h-screen relative">
         <SplineScene />
       </div>
+
+      {/* Vercel Speed Insights */}
+      <SpeedInsights />
     </div>
   )
 }
